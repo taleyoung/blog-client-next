38a9d907a00893adfc856afc27fda3e2403ed0bf
taleyoung <1090117853@qq.com>
2020-01-11 22:03:51 +0800

    fix: 😞 增加生产模式变量

--- next.config.js
+++ next.config.js
@@ -19,6 +19,11 @@ module.exports = withCss({
       "@itypings": path.resolve(clientPath, "typings"),
       "@config": path.resolve(clientPath, "config")
     };
+    console.log("config :", config);
+    console.log(
+      "config.optimization.minimizer :",
+      config.optimization.minimizer
+    );
     // config.module.rules.push({
     //   test: /\.(jpe?g|png|svg|gif|ico|webp)$/,
     //   use: [
@@ -34,31 +39,7 @@ module.exports = withCss({
     //     }
     //   ]
     // });
-    config.module.rules.push({
-      test: /\.less$/,
-      use: [
-        {
-          loader: "style-loader"
-        },
-        {
-          loader: "css-loader" // translates CSS into CommonJS
-        },
-        {
-          loader: "less-loader", // compiles Less to CSS
-          options: {
-            modifyVars: {
-              "primary-color": "red",
-              "link-color": "#1DA57A",
-              "border-radius-base": "2px"
-              // or
-              // hack: `true; @import "your-less-file-path.less";` // Override with less file
-            },
-            javascriptEnabled: true
-          }
-        }
-      ]
-      // ...other rules
-    });
+
     return config;
   }
   // })
